9920b0c7811fdf476d7fe98c05aa11e3a4e230c9
cbellian <89554715+cbellian@users.noreply.github.com>
2022-01-24 20:54:41 -0700

    added check if location correct, and if letter exists and how often in word

--- main.cpp
+++ main.cpp
@@ -3,13 +3,17 @@
 #include <fstream>
 #include <stdlib.h>
 #include <time.h>
+#include <algorithm>
+
+void letInWord(std::string,std::string, int[], int);
+void letInRightSpot(std::string,std::string, int[], int);
+void inputAndCheck(std::string, std::string);
 
 int main(int argc, char * argv[])
 {
     std::ifstream wordFileIn;
     wordFileIn.open(argv[1]);
-    if(wordFileIn.fail())
-    {
+    if(wordFileIn.fail()){
         std::cout << "File failed to open. \n";
         return -1;
     }
@@ -18,15 +22,42 @@ int main(int argc, char * argv[])
     srand(time(NULL));
     int wordLoc = rand() % 5757; // 0 - 5756 index
     int indexFill = 0;
-    while(getline(wordFileIn,word))
-    {
+    while(getline(wordFileIn,word)){
         arrOfWords[indexFill] = word;
         indexFill++;
     }
+    std::string input;
     std::string correctWord = arrOfWords[wordLoc];
-    std::string string1, string2, string3, string4, string5, string6;
-    std::cout << "First input: \n";
-    getline(std::cin, string1);
-    
+    for (int i = 5; i >= 0; i--){
+        std::cout << "Input Guess: \n";
+        getline(std::cin, input);
+        for(char &c : input){
+            c = tolower(c);
+        }
+        std::cout << input << std::endl;
+        inputAndCheck(correctWord,input);
+    }
     return 0;
+}
+
+void inputAndCheck(std::string correct, std::string input){
+    int arrayOfIn[] = {0,0,0,0,0};
+    int size = 5;
+    int arrayOfCorr[] = {0,0,0,0,0};
+    letInWord(input,correct,arrayOfIn,size);
+    letInRightSpot(input,correct,arrayOfCorr,size);
+}
+
+void letInWord(std::string Guess, std::string Correct, int arr[], int size){
+    for (int i = 0; i < Correct.size(); i++){
+        int count = std::count(Guess.begin(), Guess.end(), Correct[i]);
+        arr[i] = count;
+    }
+}
+void letInRightSpot(std::string guess, std::string correct, int arr[], int size){
+    for(int i = 0; i < correct.size(); i++){
+        if(guess[i]==correct[i]){
+            arr[i] = 1;
+        }
+    }
 }
