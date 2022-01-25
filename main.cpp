30591937678d1d7ca4d25157ad61b23b5c697db3
cbellian <89554715+cbellian@users.noreply.github.com>
2022-01-24 21:29:27 -0700

    check correct and add to display

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 #include <algorithm>
 
 void letInWord(std::string,std::string, int[], int);
-void letInRightSpot(std::string,std::string, int[], int);
-void inputAndCheck(std::string, std::string);
+void letInRightSpot(std::string,std::string, char[], int[], int);
+bool inputAndCheck(std::string, std::string);
+bool winConditions(bool[], int);
 
 int main(int argc, char * argv[])
 {
@@ -34,30 +35,61 @@ int main(int argc, char * argv[])
         for(char &c : input){
             c = tolower(c);
         }
-        std::cout << input << std::endl;
-        inputAndCheck(correctWord,input);
+        std::cout << correctWord << std::endl;
+        bool win = inputAndCheck(correctWord,input);
+        if(win)
+        {
+            std::cout << "WIN \n";
+            return 1;
+        }
     }
     return 0;
 }
-
-void inputAndCheck(std::string correct, std::string input){
-    int arrayOfIn[] = {0,0,0,0,0};
-    int size = 5;
-    int arrayOfCorr[] = {0,0,0,0,0};
-    letInWord(input,correct,arrayOfIn,size);
-    letInRightSpot(input,correct,arrayOfCorr,size);
-}
-
 void letInWord(std::string Guess, std::string Correct, int arr[], int size){
     for (int i = 0; i < Correct.size(); i++){
         int count = std::count(Guess.begin(), Guess.end(), Correct[i]);
         arr[i] = count;
     }
 }
-void letInRightSpot(std::string guess, std::string correct, int arr[], int size){
+void letInRightSpot(std::string guess, std::string correct, char wordSpot[], bool arr[], int size){
     for(int i = 0; i < correct.size(); i++){
         if(guess[i]==correct[i]){
             arr[i] = 1;
         }
     }
+    for(int i = 0; i < size; i++){
+        if(arr[i] == 1){
+            wordSpot[i] = correct[i];
+        }
+        else if(arr[i] == 0){
+            wordSpot[i] = '_';
+        }
+    }
+}
+bool winConditions(bool correct[], int size){
+    int count = std::count(correct, correct + 5, 1);
+    if (count == 5)
+    {
+        return 1;
+    }
+    return 0;
+}
+bool inputAndCheck(std::string correct, std::string input){
+    int arrayOfIn[] = {0,0,0,0,0};
+    int size = 5;
+    bool arrayOfCorr[] = {0,0,0,0,0};
+    char wordRight[5];
+    letInWord(input,correct,arrayOfIn,size);
+    letInRightSpot(input,correct,wordRight,arrayOfCorr,size);
+    bool win = winConditions(arrayOfCorr,size);
+    if (win)
+    {
+        return 1;
+    }
+    else{
+        for(int i = 0; i<size;i++){
+            std::cout << wordRight[i];
+        }
+    }
+    return 0;
 }
